9f598ea29b957d6ccb95b6af590a599edfcafd12
Doreen Chemweno <doreenjeptoo@gmail.com>
2023-10-04 07:10:27 +0300

    added tenum talk 10

---
+++ tenumTalks10/21_merge_two_sorted_lists_leetcode/js/91_36_Ginger-Stone.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  const list3 = new ListNode();
+  let current = list3;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  if (list1 !== null) {
+    current.next = list1;
+  } else {
+    current.next = list2;
+  }
+
+  return list3.next; // The merged list starts from the next of the list3 node
+};
